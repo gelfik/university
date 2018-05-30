69598f866aef9bc6db54aef5a99aa5f8546a98f8
Bulat Gilfanov <bulatgilfanov@gmail.com>
2018-05-31 01:05:15 +0300

    Add files via upload

---
+++ 2course/task9/rectangle_figure.pas
@@ -0,0 +1,35 @@
+unit Rectangle_Figure;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Math, Figure;
+
+type
+  TRectangle = class(TFigure)
+  private
+    r, h: integer;
+  public
+    constructor Create(ax, ay: integer);
+    procedure Show; override;
+  end;
+
+implementation
+
+uses unit1;
+
+constructor TRectangle.Create(ax, ay: integer);
+begin
+  inherited Create(ax, ay);
+  r := randomrange(50, 100);
+  h := randomrange(50, 150);
+end;
+
+procedure TRectangle.Show;
+begin
+  form1.image1.canvas.Rectangle(x, y, x + r, y + h);
+end;
+
+end.
